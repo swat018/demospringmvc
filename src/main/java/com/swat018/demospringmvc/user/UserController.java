eed2f0111f17210074adbcef5e5867f12089ec32
jinwoo park <swat018@gmail.com>
2020-05-31 14:43:39 +0900

    스프링 웹 MVC - 소개

--- src/main/java/com/swat018/demospringmvc/user/UserController.java
+++ src/main/java/com/swat018/demospringmvc/user/UserController.java
@@ -1,4 +1,13 @@
 package com.swat018.demospringmvc.user;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "hello";
+    }
 }
--- src/test/java/com/swat018/demospringmvc/user/UserControllerTest.java
+++ src/test/java/com/swat018/demospringmvc/user/UserControllerTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
@@ -15,7 +17,10 @@ public class UserControllerTest {
     MockMvc mockMvc;
 
     @Test
-    public void hello() {
+    public void hello() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("hello"));
     }
 
 }
