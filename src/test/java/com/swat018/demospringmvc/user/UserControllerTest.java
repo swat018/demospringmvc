d7307cffd91cab852a90fde71c7a102cc98dd513
jinwoo park <swat018@gmail.com>
2020-05-31 15:22:37 +0900

    스프링 웹 MVC - HttpMessageConverters

--- src/test/java/com/swat018/demospringmvc/user/UserControllerTest.java
+++ src/test/java/com/swat018/demospringmvc/user/UserControllerTest.java
@@ -4,10 +4,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(UserController.class)
@@ -23,4 +27,18 @@ public class UserControllerTest {
                 .andExpect(MockMvcResultMatchers.content().string("hello"));
     }
 
+    @Test
+    public void createUser_JSON() throws Exception{
+        String userJson = "{\"username\":\"jinwoo\", \"password\":\"123\"}";
+        mockMvc.perform(MockMvcRequestBuilders.post("/users/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(userJson))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.username",
+                            is(equalTo("jinwoo"))))
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.password",
+                            is(equalTo("123"))));
+
+    }
 }
