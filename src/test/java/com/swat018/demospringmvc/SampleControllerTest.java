27f7a1f1a361222f270b672dd47c0d5e0fa79503
jinwoo park <swat018@gmail.com>
2020-06-03 09:09:21 +0900

    스프링 웹 MVC - HtmlUnit

--- src/test/java/com/swat018/demospringmvc/SampleControllerTest.java
+++ src/test/java/com/swat018/demospringmvc/SampleControllerTest.java
@@ -1,32 +1,30 @@
 package com.swat018.demospringmvc;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.is;
-
-
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(SampleController.class)
 public class SampleControllerTest {
 
     @Autowired
+    WebClient webClient;
+
+    @Autowired
     MockMvc mockMvc;
 
     @Test
     public void hello() throws Exception {
-        // 요청 "/hello"
+/*        // 요청 "/hello"
         // 응답
         // - 모델 name : jinwoo
         // - 뷰 이름 : hello
@@ -35,6 +33,10 @@ public class SampleControllerTest {
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.view().name("hello"))
                 .andExpect(MockMvcResultMatchers.model().attribute("name", is("jinwoo")))
-                .andExpect(MockMvcResultMatchers.content().string(containsString("jinwoo")));
+                .andExpect(MockMvcResultMatchers.xpath("//h1").string("jinwoo"))
+                .andExpect(MockMvcResultMatchers.content().string(containsString("jinwoo")));*/
+        HtmlPage page = webClient.getPage("/hello");
+        HtmlHeading1 h1 = page.getFirstByXPath("//h1");
+        assertThat(h1.getTextContent()).isEqualToIgnoringCase("jinwoo");
     }
 }
