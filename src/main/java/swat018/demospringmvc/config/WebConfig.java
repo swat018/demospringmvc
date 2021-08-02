9d4b53702bb3b4da713ca74a0ff90dbad4832a58
jinwoo park <swat018@icloud.com>
2021-08-02 23:12:51 +0900

    Initial commit

---
+++ src/main/java/swat018/demospringmvc/config/WebConfig.java
@@ -0,0 +1,10 @@
+package swat018.demospringmvc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+}
---
+++ src/test/java/swat018/demospringmvc/DemospringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package swat018.demospringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospringmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
