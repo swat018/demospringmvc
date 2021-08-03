8c0b003adf76747af707889f141797740db99c6c
jinwoo park <swat018@gmail.com>
2021-08-03 11:47:07 +0900

    스프링 웹 MVC 4부: 정적 리소스 지원

--- src/main/java/swat018/demospringmvc/config/WebConfig.java
+++ src/main/java/swat018/demospringmvc/config/WebConfig.java
@@ -2,9 +2,16 @@ package swat018.demospringmvc.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/m/**")
+                .addResourceLocations("classpath:/m/")
+                .setCachePeriod(20);
+    }
 }
